21a151b2cba9cdc0ded8506528aa072487eac9de
venu reddy <venugopalreddy9493@gmail.com>
2023-10-18 21:44:42 +0530

    18commit6

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -13,23 +13,6 @@ import Offers from "./Offers.tsx"
 // import { SyncLoader } from "react-spinners"
 
 const HomePage = () => {
-  // const [value, setValue] = useState<any>([])
-  // var formData = new FormData()
-  // const handleSendBtn = async () => {
-  //   const res = await axios.post('http://localhost:5000/api/product/upload-to-s3',
-  //     formData, { headers: { 'Content-Type': 'multipart/form-data' } })
-  //   console.log(res.data);
-  //   setValue(res.data?.url)
-  //   formData = new FormData();
-  // }
-  // const handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   if (e.target && e.target.files) {
-  //     for (let i = 0; i < e.target.files.length; i++) {
-  //       formData.append("images", e.target.files[i])
-  //     }
-  //   }
-  // }
-
   return (
     <>
       <div className="bg-[#FFFFF7]">
@@ -41,11 +24,6 @@ const HomePage = () => {
         <SpecialCart />
         <PopularProduct />
         <BrandsAndBlogs />
-        {/* <div>
-          <input multiple onChange={handlechange} type="file" name="fileinput" id="" />
-          <button onClick={handleSendBtn} className="px-3 py-2 border shadow-md bg-gray-200 rounded-md hover:shadow-lg">sendImage</button>
-          <img src={value} alt="awsImage" />
-        </div> */}
       </div>
     </>
   )
