8ba0ea5f2a0fe2f369ce5e642352f2b425134be5
venu123143 <venugopalreddy9493@gmail.com>
2023-11-09 15:11:42 +0530

    amaozon clone v1 done

--- src/components/Product/Blogs.tsx
+++ src/components/Product/Blogs.tsx
@@ -22,7 +22,7 @@ const Blogs = () => {
     <section className="bg-skin-background">
       <div className="sm:px-5">
         <div className="w-full flex justify-center ">
-          <div className={`w-3/12 my-5 hidden 1100px:block`}>
+          {/* <div className={`w-3/12 my-5 hidden 1100px:block`}>
             <div className="text-skin-base bg-skin-background rounded-lg sticky top-30  py-[10px] px-[15px] mb-3 shadow-lg">
               <h3 className="text-skin-base text-[1rem] font-[600] space-x-2 mb-[20px] ">Shop By Categories</h3>
               <div className="h-[150px] overflow-y-scroll no-scrollbar">
@@ -38,11 +38,10 @@ const Blogs = () => {
                 </ul>
               </div>
             </div>
-          </div>
-          <div className="1100px:w-9/12 my-5">
+          </div> */}
+          <div className="my-5">
             <h3 className="font-[550] text-[1.5rem] text-skin-base hover:underline w-fit md:hidden m-auto"> Blogs</h3>
-
-            <div className="grid 800px:grid-cols-2 grid-cols-1 gap-5 place-items-center">
+            <div className="grid 800px:grid-cols-2 sm:grid-cols-1 gap-5 place-items-center">
               {blogs?.map((each, index) => (
                 <BigBlogCard key={index} blog={each} />
               ))}
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,10 @@
+import React from "react"
 import { AiOutlineHome, AiOutlineMail, AiOutlineInfoCircle } from "react-icons/ai"
 import { BsTelephone } from "react-icons/bs"
 const Contact = () => {
+    const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
     return (
         <section className="bg-[#FFFFF7] dark:bg-[#323436]">
             <section className="px-4 sm:px-10 py-5">
@@ -10,7 +14,7 @@ const Contact = () => {
                 <div className="block 1100px:flex justify-evenly my-5 bg-white dark:bg-[#222e35] p-5 shadow-lg">
                     <div>
                         <h3 className="font-bold text-2xl text-skin-base">Contact</h3>
-                        <form action="" method="post" className="mt-[1rem]">
+                        <form action="" method="post" className="mt-[1rem]" onSubmit={handleForm}>
                             <div className="mb-6 w-full">
                                 <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full sm:max-w-[50%] 1100px:max-w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="enter your name" />
@@ -26,7 +30,7 @@ const Contact = () => {
                             <div className="mb-6 w-full">
                                 <textarea name="" id="" cols={40} rows={4} placeholder="comments " className="rounded-lg p-2.5  w-full sm:max-w-[50%] 1100px:max-w-full  bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" ></textarea>
                             </div>
-                            <button className="button dark:bg-[#00a884] my-[10px] text-white text-[0.91rem] px-[25px] py-[6px] rounded-[25px]">
+                            <button type="submit" className="button dark:bg-[#00a884] my-[10px] text-white text-[0.91rem] px-[25px] py-[6px] rounded-[25px]">
                                 Submit
                             </button>
                         </form>
